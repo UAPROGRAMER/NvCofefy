9933312ab25466e9209b48b6f6215f7d2697021c
Stas Yatskiv <stasyatskiu2008@gmail.com>
2025-01-21 23:01:38 +0200

    Some things changed

--- lua/NvCofefy/plugins/comment.lua
+++ lua/NvCofefy/plugins/comment.lua
@@ -1,1 +1,7 @@
-return { "numToStr/Comment.nvim" }
+return {
+    "numToStr/Comment.nvim",
+    config = function()
+        vim.keymap.set('n', '<leader>cc', 'gcc', { desc = vim.fn.nr2char(0xf27a).."[C]ode [C]oment" })
+        vim.keymap.set('v', 'cc', 'gc', { desc = vim.fn.nr2char(0xf27a).."[C]ode [C]oment" })
+    end
+}
